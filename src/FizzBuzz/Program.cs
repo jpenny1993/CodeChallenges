79ae798a32180576402372bdb557716b05a8d477
John Penny <jpenny1993@users.noreply.github.com>
2019-12-26 13:19:08 +0000

    normalize string concatenations

--- src/FizzBuzz/Program.cs
+++ src/FizzBuzz/Program.cs
@@ -17,19 +17,18 @@ namespace FizzBuzz
                 Console.WriteLine(result);
             }
         }
-
+        
         private static string DoFizzBuzz(int value)
         {
-            var fizz = value % 3 == 0;
-            var buzz = value % 5 == 0;
-
-            var str = string.Empty;
-             
-            if (fizz) 
-                str += "Fizz";
+            var fizz = value % 3 == 0 
+                ? "Fizz"
+                : string.Empty;
             
-            if (buzz)
-                str += "Buzz";
+            var buzz = value % 5 == 0
+                ? "Buzz"
+                : string.Empty;
+            
+            var str = string.Concat(fizz, buzz);
             
             return string.IsNullOrEmpty(str)
                 ? value.ToString() 
